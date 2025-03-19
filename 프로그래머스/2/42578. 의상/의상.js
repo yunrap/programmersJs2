0f795fe575b0f01e956c1f0e3c0b492986c04468
yun yeji <83654790+yunrap@users.noreply.github.com>
2025-03-19 22:57:16 +0900

    [level 2] Title: 의상, Time: 4.32 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,24 +1,22 @@
-// 수민님푼문제 참고하였다, hashset 구현
-// 중복되는데이터를 [_,type] 로 뺄수있다는사실!!.
-// 저번에는 data[1] 자리수로 복잡하게준거 리팩토링
-
 function solution(clothes) {
-  var answer = 1;
-  let clotheMap = new Map();
-
-  clothes.forEach(([_,type]) => {
-    if (clotheMap.has(type)) {
-      clotheMap.set(type, clotheMap.get(type) + 1);
-    } else {
-      clotheMap.set(type, 1);
-    }
-  });
-
-  for (let a of clotheMap.values()) {
-    console.log(a);
-    answer *= a + 1;
-  }
+    const answer = new Map();
     
-  answer--;
-  return answer;
+    for(data of clothes){
+        let value;
+        const keys = data[1];
+        if(answer.has(keys)){
+           const value = answer.get(keys);
+           answer.set(keys, value+1);
+        }else{
+           answer.set(keys, 1);
+        }
+    }
+    
+    let result = 1;
+    for (const count of answer.values()) {
+        console.log(count)
+        result *= (count + 1); 
+    }
+    
+    return result - 1;
 }
